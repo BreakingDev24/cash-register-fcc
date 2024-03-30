44fc2c4562641460694052d7a07ce8d10268cc44
BreakingDev24 <102031294+BreakingDev24@users.noreply.github.com>
2024-03-30 10:25:52 +0100

    add document variables

---
+++ script.js
@@ -0,0 +1,6 @@
+//VARIABLES
+const changeDueContainer = document.getElementById('change-due');
+const cashInput = document.getElementById('cash');
+const purchaseBtn = document.getElementById('purchase-btn');
+const priceScreen = document.getElementById('price-screen');
+const chidMoney = document.getElementById('cid-money');
